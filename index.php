de429239e5f06c2e8bb32184351e401d7debadac
William Venner <root@blogs.protectedhosting.co.uk>
2016-06-22 13:28:41 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>LOGS</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+		<script type="text/javascript" src="assets/js/lib/jquery-3.0.0.min.js"></script>
+		<link rel="icon" href="assets/img/accept.png" type="image/png" />
+	</head>
+	<body>
+		<div class="logs"></div>
+		<!--<div class="logs"></div>-->
+		<div class="controls"><div>
+			<div class="button" id="clear_errorlogs">Clear error log</div>
+			<div class="button" id="refresh_errorlogs">Refresh error log</div>
+		</div></div>
+		<!--<div class="controls"><div>
+			<div class="button" id="clear_accesslogs">Clear access log</div>
+			<div class="button" id="refresh_accesslogs">Refresh access log</div>
+		</div></div>-->
+		<script type="text/javascript" src="assets/js/logs.js"></script>
+	</body>
+</html>
